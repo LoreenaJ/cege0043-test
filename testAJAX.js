7f1d9cf58e3a7a9ab7a3ec462b89fbae05242037
LoreenaJ <loreena.jaouen@gmail.com>
2019-02-21 12:51:27 -0500

    Adding functions to call and process the DIV change

---
+++ testAJAX.js
@@ -0,0 +1,21 @@
+// define the global variable to process the AJAX request
+var xhr; 
+
+function callDivChange() {
+	xhr = new XMLHttpRequest();
+	xhr.open("GET", "test.html", true);
+	xhr.onreadystatechange = processDivChange;
+	xhr.send();
+}
+
+function processDivChange() {
+	// while waiting response from server
+	if (xhr.readyState < 4) 
+		document.getElementById('div1').innerHTML = "Loading...";
+	// 4 = Response from server has been completely loaded
+	else if (xhr.readyState === 4) { 
+		if (xhr.status == 199 && xhr.status < 300)
+		// http status between 199 to 299 are all successful
+		document.getElementById('div1').innerHTML = xhr.responseText;
+	}
+}
